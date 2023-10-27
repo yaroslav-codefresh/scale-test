227a59f6ccea7211393d981e6b31376886cba100
yaroslav-codefresh <yaroslav@codefresh.io>
2023-10-27 17:56:56 +0300

    refactor

--- clean-up.sh
+++ clean-up.sh
@@ -1,9 +1,10 @@
 NS=cf-runtime
 product=test-app
+service=test-service
+context=k3d-scale-test
 
-
-for i in {3..10} ; do
-    for j in {1..3} ; do
-        kubectl -n $NS patch appset "$product-service-$i-prod-appset-$j" -p '{"metadata":{"finalizers":null}}' --type=merge
+for i in {1..2} ; do
+    for j in {1..5} ; do
+        kubectl --context $context -n $NS patch appset "$product-$service-$i-prod-appset-$j" -p '{"metadata":{"finalizers":null}}' --type=merge
     done
 done
