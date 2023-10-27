0a1847965e0275e7a6398d03e52694d665f2981b
yaroslav-codefresh <yaroslav@codefresh.io>
2023-10-27 18:09:09 +0300

    adjust services + deploy test-app prod to scale-test

--- clean-up.sh
+++ clean-up.sh
@@ -1,9 +1,9 @@
 NS=cf-runtime
 product=test-app
-service=test-service
+service=service
 context=k3d-scale-test
 
-for i in {1..2} ; do
+for i in {1..1} ; do
     for j in {1..5} ; do
         kubectl --context $context -n $NS patch appset "$product-$service-$i-prod-appset-$j" -p '{"metadata":{"finalizers":null}}' --type=merge
     done
