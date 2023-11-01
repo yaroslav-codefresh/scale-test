79c0a1c4923dd0daf8e3df345283e31a9c00fedb
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2023-11-01 11:08:21 +0200

    product 2с (60apps)

--- clean-up.sh
+++ clean-up.sh
@@ -1,9 +1,9 @@
-NS=cf-runtime
-product=test-app
+NS=argo-load
+product=product-a
 service=service
-context=k3d-scale-test
+context=arn:aws:eks:us-east-1:882458141806:cluster/argo-load
 
-for i in {1..1} ; do
+for i in {3..3} ; do
     for j in {1..5} ; do
         kubectl --context $context -n $NS patch appset "prod-$product-$service-$i-appset-$j" -p '{"metadata":{"finalizers":null}}' --type=merge
     done
