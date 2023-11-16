00800e46534ef5dc52377a667a1a3549cd4e1620
yaroslav-codefresh <yaroslav@codefresh.io>
2023-11-16 19:28:58 +0200

    add app

---
+++ install-sandbox.sh
@@ -0,0 +1,11 @@
+helm upgrade --install cf-gitops-runtime \
+  --create-namespace \
+  --namespace yarik \
+  --set global.codefresh.accountId=6422abe703c720761a07f3e0 \
+  --set global.codefresh.userToken.token=6556389122bf66c162069708.1e667a8163dfa100d418c87b566335e2 \
+  --set global.runtime.name=yarik \
+  --set global.codefresh.url=https://sandbox-1.codefresh.io \
+  --set tunnel-client.tunnelServer.host=register-tunnels.non-prod-ue1-sandbox-1.cf-op.com \
+  --set tunnel-client.tunnelServer.subdomainHost=tunnels.non-prod-ue1-sandbox-1.cf-op.com \
+  --kube-context k3d-local-runtime \
+  cf-gitops-runtime/gitops-runtime
