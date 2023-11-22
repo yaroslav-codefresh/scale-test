29904b73710a6e49bc6d2c60a193b30045c6e166
yaroslav-codefresh <yaroslav@codefresh.io>
2023-11-22 19:16:33 +0200

    add app

--- install-sandbox.sh
+++ install-sandbox.sh
@@ -5,6 +5,7 @@ helm upgrade --install cf-gitops-runtime \
   --set global.codefresh.userToken.token=6556389122bf66c162069708.1e667a8163dfa100d418c87b566335e2 \
   --set global.runtime.name=yarik \
   --set global.codefresh.url=https://sandbox-1.codefresh.io \
+  --set argo-cd.configs.cm."application.resourceTrackingMethod"=label \
   --set tunnel-client.tunnelServer.host=register-tunnels.non-prod-ue1-sandbox-1.cf-op.com \
   --set tunnel-client.tunnelServer.subdomainHost=tunnels.non-prod-ue1-sandbox-1.cf-op.com \
   --kube-context k3d-local-runtime \
