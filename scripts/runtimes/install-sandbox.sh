3d91b9425ba918051cd23edb2972bcd97619e1fb
yaroslav-codefresh <yaroslav@codefresh.io>
2023-11-27 16:56:05 +0200

    refactor

--- scripts/runtimes/install-sandbox.sh
+++ scripts/runtimes/install-sandbox.sh
@@ -2,6 +2,6 @@ helm upgrade --install cf-gitops-runtime \
   --create-namespace \
   --debug \
   --namespace yarik \
-  --values /Users/siloenix/work/codefresh/scale-test/sandbox-values.yaml \
+  --values /Users/siloenix/work/codefresh/scale-test/values/sandbox-1-values.yaml \
   --kube-context k3d-local-runtime \
   cf-gitops-runtime/gitops-runtime | tee debug.log
