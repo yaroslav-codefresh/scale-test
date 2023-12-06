6c276cc2a565009620bb5e24cb2a04e227fccb75
yaroslav-codefresh <yaroslav@codefresh.io>
2023-12-06 23:15:27 +0200

    use default guestbook

--- scripts/runtimes/util/current-context.sh
+++ scripts/runtimes/util/current-context.sh
@@ -1,10 +1,10 @@
 # for testing new reporter
-#using_context "dev-chart__local-runtime__yaroslav-codefresh__new-reporter.sh"
+using_context "dev-chart__local-runtime__yaroslav-codefresh__new-reporter.sh"
 
 # for sandbox-1
 #using_context "prod-chart__local-runtime__codefresh-inc__sandbox-1.sh"
 
 # for testing agro-load
-using_context "dev-chart__argo-load__oleksandr-codefresh__argo-load.sh"
+#using_context "dev-chart__argo-load__oleksandr-codefresh__argo-load.sh"
 
 validate_required_env
