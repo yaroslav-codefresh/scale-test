9cd60848a808d78f2b9db2fe39d7c43772b8a227
yaroslav-codefresh <yaroslav@codefresh.io>
2023-12-07 17:06:20 +0200

    fix script

--- scripts/setup/prepare_aws_cluster.sh
+++ scripts/setup/prepare_aws_cluster.sh
@@ -1,8 +1,8 @@
 export AWS_PROFILE="cd-team"
-aws sso login --profile ${AWS_PROFILE}
+#aws sso login --profile ${AWS_PROFILE}
 
 export AWS_DEFAULT_REGION="us-east-1"
-export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
+export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --profile cd-team --query Account --output text)"
 
 export KUBERNETES_VERSION=1.27
 export KARPENTER_VERSION=v0.28.0
